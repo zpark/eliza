eee9f996b5d4e89df4d292e1628449034b8406ea
snow-farmer <80384716+snow-farmer@users.noreply.github.com>
2024-12-04 19:28:17 +0100

    token mill create token action

--- packages/plugin-avalanche/src/index.ts
+++ packages/plugin-avalanche/src/index.ts
@@ -1,16 +1,18 @@
 import { Plugin } from "@ai16z/eliza";
+import tokenMillCreate from "./actions/tokenMillCreate";
 import transfer from './actions/transfer'
 import yakSwap from './actions/yakSwap'
 import yakStrategy from './actions/yakStrategy'
 import { tokensProvider } from './providers/tokens'
 import { strategiesProvider } from './providers/strategies'
 import { walletProvider } from './providers/wallet'
-import { TOKEN_ADDRESSES, STRATEGY_ADDRESSES, YAK_SWAP_CONFIG } from './utils/constants'
+import { TOKEN_ADDRESSES, STRATEGY_ADDRESSES, YAK_SWAP_CONFIG, TOKEN_MILL_CONFIG } from './utils/constants'
 
 export const PROVIDER_CONFIG = {
     TOKEN_ADDRESSES: TOKEN_ADDRESSES,
     STRATEGY_ADDRESSES: STRATEGY_ADDRESSES,
-    YAK_SWAP_CONFIG: YAK_SWAP_CONFIG
+    YAK_SWAP_CONFIG: YAK_SWAP_CONFIG,
+    TOKEN_MILL_CONFIG: TOKEN_MILL_CONFIG
 }
 
 export const avalanchePlugin: Plugin = {
@@ -19,7 +21,8 @@ export const avalanchePlugin: Plugin = {
     actions: [
         transfer,
         yakSwap,
-        yakStrategy
+        yakStrategy,
+        tokenMillCreate
     ],
     evaluators: [],
     providers: [
--- packages/plugin-avalanche/src/types/index.ts
+++ packages/plugin-avalanche/src/types/index.ts
@@ -7,4 +7,29 @@ interface YakSwapQuote {
     gasEstimate: bigint;
 }
 
-export type { YakSwapQuote }
+// struct MarketCreationParameters {
+//     uint96 tokenType;
+//     string name;
+//     string symbol;
+//     address quoteToken;
+//     uint256 totalSupply;
+//     uint16 creatorShare;
+//     uint16 stakingShare;
+//     uint256[] bidPrices;
+//     uint256[] askPrices;
+//     bytes args;
+// }
+interface TokenMillMarketCreationParameters {
+    tokenType: number;
+    name: string;
+    symbol: string;
+    quoteToken: Address;
+    totalSupply: bigint;
+    creatorShare: number;
+    stakingShare: number;
+    bidPrices: bigint[];
+    askPrices: bigint[];
+    args: string;
+}
+
+export type { YakSwapQuote, TokenMillMarketCreationParameters }
