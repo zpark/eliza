7d81e3913e3d8653742b113f9a04d4f79519fa06
oxSaturn <oxSaturn@proton.me>
2024-12-21 13:31:38 +0800

    fix: set default value for cache store

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -647,7 +647,7 @@ async function startAgent(
         await db.init();
 
         const cache = initializeCache(
-            process.env.CACHE_STORE,
+            process.env.CACHE_STORE ?? CacheStore.DATABASE,
             character,
             "",
             db
