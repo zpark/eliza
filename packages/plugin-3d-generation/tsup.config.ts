2e2e074949f1b6250cb9448bbffc3fc014dc9b9e
root <root@localhost.localdomain>
2024-12-25 14:41:24 +0800

    add fal ai

--- packages/plugin-3d-generation/tsup.config.ts
+++ packages/plugin-3d-generation/tsup.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
         "process",
         "@reflink/reflink",
         "@node-llama-cpp",
+        "@fal-ai/client",
         "https",
         "http",
         "agentkeepalive",
