09b25e67f45be277fb92c4bd8cbe23dda300555d
Ed-Marcavage <edmarcavage256@proton.me>
2024-12-26 19:39:44 -0500

    change repo owner to elizaOS

--- packages/jsdoc-automation/src/Configuration.ts
+++ packages/jsdoc-automation/src/Configuration.ts
@@ -39,7 +39,7 @@ export class Configuration implements Omit<ConfigurationData, 'rootDirectory'> {
 
     public excludedDirectories: string[] = [];
     public repository: Repository = {
-        owner: 'ai16z',
+        owner: 'elizaOS',
         name: 'eliza',
         pullNumber: undefined
     };
