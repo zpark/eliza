674eab0df68e6447bab7f1aeab631ab42df68468
Ron Turetzky <turetzkyron@gmail.com>
2025-01-01 19:09:13 -0500

    chore: rename

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1326,7 +1326,7 @@ export enum TranscriptionProvider {
  * Available verifiable inference providers
  */
 export enum VerifiableInferenceProvider {
-    RECLAIM = "opacity",
+    OPACITY = "opacity",
 }
 
 /**
