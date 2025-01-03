9b47b747eeaf10530f3b86c3e1d652e2c23ce382
Shakker Nerd <shakkerdroid@gmail.com>
2025-01-03 03:54:44 +0000

    chore: remove double imports

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -7,7 +7,6 @@ import { LensAgentClient } from "@elizaos/client-lens";
 import { SlackClientInterface } from "@elizaos/client-slack";
 import { TelegramClientInterface } from "@elizaos/client-telegram";
 import { TwitterClientInterface } from "@elizaos/client-twitter";
-import { webSearchPlugin } from "@elizaos/plugin-web-search";
 import {
     AgentRuntime,
     CacheManager,
