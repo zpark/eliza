e05f6d9771f6a19ae559f7dcf8c4a0feed6a1a17
Daniel Farina <m3d.webdev@gmail.com>
2025-01-05 14:19:29 +0700

    Add Stargaze plugin

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -64,6 +64,7 @@ import { abstractPlugin } from "@elizaos/plugin-abstract";
 import { avalanchePlugin } from "@elizaos/plugin-avalanche";
 import { webSearchPlugin } from "@elizaos/plugin-web-search";
 import { echoChamberPlugin } from "@elizaos/plugin-echochambers";
+import { stargazePlugin } from "@elizaos/plugin-stargaze";
 import Database from "better-sqlite3";
 import fs from "fs";
 import path from "path";
@@ -609,6 +610,7 @@ export async function createAgent(
             getSecret(character, "ECHOCHAMBERS_API_KEY")
                 ? echoChamberPlugin
                 : null,
+            getSecret(character, "STARGAZE_ENDPOINT") ? stargazePlugin : null,
         ].filter(Boolean),
         providers: [],
         actions: [],
