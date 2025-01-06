c751b23933925c26cc0e6e24f69233b75c124614
Joey <joey.kheireddine@outlook.com>
2025-01-06 18:54:15 +0100

    upd: created initial api routes file

---
+++ client/app/lib/api.ts
@@ -0,0 +1,48 @@
+const BASE_URL = "http://localhost:3000";
+
+const fetcher = async ({
+    url,
+    method,
+    body,
+}: {
+    url: string;
+    method?: "GET" | "POST";
+    body?: object;
+}) => {
+    const options: RequestInit = {
+        method: method ?? "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+    };
+
+    if (method === "POST" && body) {
+        options.body = JSON.stringify(body);
+    }
+
+    return fetch(`${BASE_URL}${url}`, options).then(async (resp) => {
+        if (resp.ok) {
+            return resp.json();
+        }
+
+        const errorText = await resp.text();
+        console.error("Error: ", errorText);
+
+        let errorMessage = "An error occurred.";
+        try {
+            const errorObj = JSON.parse(errorText);
+            errorMessage = errorObj.message || errorMessage;
+        } catch {
+            errorMessage = errorText || errorMessage;
+        }
+
+        throw new Error(errorMessage);
+    });
+};
+
+export const apiClient = {
+    sendMessage: (agentId: string) =>
+        fetcher({ url: `/${agentId}/message`, method: "POST" }),
+    getAgents: () => fetcher({ url: "/agents" }),
+};
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,22 +3,22 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
+    interface Future {
+        v3_singleFetch: true;
+    }
 }
 
 export default defineConfig({
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+    plugins: [
+        remix({
+            future: {
+                v3_fetcherPersist: true,
+                v3_relativeSplatPath: true,
+                v3_throwAbortReason: true,
+                v3_singleFetch: true,
+                v3_lazyRouteDiscovery: true,
+            },
+        }),
+        tsconfigPaths(),
+    ],
 });
