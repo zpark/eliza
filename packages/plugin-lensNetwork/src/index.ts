f4fa049c0f8796f34979110c0170bffc7c67ec5c
pranjallyad <pranjalyadavtricks@gmail.com>
2025-01-10 14:34:28 +0530

    Added Lens Testnet Network Support

--- packages/plugin-lensNetwork/src/index.ts
+++ packages/plugin-lensNetwork/src/index.ts
@@ -1,12 +1,12 @@
 import { Plugin } from "@elizaos/core";
 
 import transfer from "./actions/transfer.ts";
-import trasferFromWallet from "./actions/trasferFromWallet.ts";
+
 
 export const LensPlugin: Plugin = {
     name: "Lens",
     description: "Lens Plugin for Eliza",
-    actions: [transfer,trasferFromWallet],
+    actions: [transfer],
     evaluators: [],
     providers: [],
 };
