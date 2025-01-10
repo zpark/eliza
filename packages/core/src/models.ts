80594ec1d5efc8a520184756c14ba0c43e783fea
brauliolomeli <brauliolomeli@gmail.com>
2025-01-10 10:41:37 -0600

    add support for Mistral model in text generation

--- packages/core/src/models.ts
+++ packages/core/src/models.ts
@@ -377,6 +377,31 @@ export const models: Models = {
             },
         },
     },
+    [ModelProviderName.MISTRAL]: {
+        settings: {
+            stop: [],
+            maxInputTokens: 128000,
+            maxOutputTokens: 8192,
+            frequency_penalty: 0.4,
+            presence_penalty: 0.4,
+            temperature: 0.7,
+        },
+        model: {
+            [ModelClass.SMALL]:
+                settings.SMALL_MISTRAL_MODEL ||
+                settings.MISTRAL_MODEL ||
+                "mistral-small-latest",
+            [ModelClass.MEDIUM]:
+                settings.MEDIUM_MISTRAL_MODEL ||
+                settings.MISTRAL_MODEL ||
+                "mistral-large-latest",
+            [ModelClass.LARGE]:
+                settings.LARGE_MISTRAL_MODEL ||
+                settings.MISTRAL_MODEL ||
+                "mistral-large-latest",
+            [ModelClass.EMBEDDING]: "mistral-embed",
+        },
+    },
     [ModelProviderName.REDPILL]: {
         endpoint: "https://api.red-pill.ai/v1",
         // Available models: https://docs.red-pill.ai/get-started/supported-models
