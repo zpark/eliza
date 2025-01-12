1adebb2d10ce8c7186a82a823caf8b7ebc217db2
Ting Chien Meng <tcm390@nyu.edu>
2025-01-11 21:56:42 -0500

    update import

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -101,7 +101,7 @@ import net from "net";
 import path from "path";
 import { fileURLToPath } from "url";
 import yargs from "yargs";
-import { createNFTCollectionsPlugin } from "@elizaos/plugin-nft-collections";
+import createNFTCollectionsPlugin from "@elizaos/plugin-nft-collections";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
