6110782963008ec25a3a14dca9770a29e05bc929
Max Coto <maxiperezc@gmail.com>
2025-01-20 10:47:24 -0300

    feat: add a way to create/store/restore agents in the filesystem (#2389)
    * Add a way to create/store/restore agents in the filesystem at agents/data/characters
    
    * Update .env.example
    
    ---------
    
    Co-authored-by: Sayo <hi@sayo.wtf>

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -389,10 +389,31 @@ function commaSeparatedStringToArray(commaSeparated: string): string[] {
     return commaSeparated?.split(",").map((value) => value.trim());
 }
 
+async function readCharactersFromStorage(characterPaths: string[]): Promise<string[]> {
+    try {
+        const uploadDir = path.join(process.cwd(), "data", "characters");
+        await fs.promises.mkdir(uploadDir, { recursive: true });
+        const fileNames = await fs.promises.readdir(uploadDir);
+        fileNames.forEach(fileName => {
+            characterPaths.push(path.join(uploadDir, fileName));
+        });
+    } catch (err) {
+        elizaLogger.error(`Error reading directory: ${err.message}`);
+    }
+
+    return characterPaths;
+};
+
 export async function loadCharacters(
     charactersArg: string
 ): Promise<Character[]> {
-    const characterPaths = commaSeparatedStringToArray(charactersArg);
+
+    let characterPaths = commaSeparatedStringToArray(charactersArg);
+
+    if(process.env.USE_CHARACTER_STORAGE === "true") {
+        characterPaths = await readCharactersFromStorage(characterPaths);
+    }
+
     const loadedCharacters: Character[] = [];
 
     if (characterPaths?.length > 0) {
@@ -1248,7 +1269,9 @@ const startAgents = async () => {
         characters = await loadCharacterFromOnchain();
     }
 
-    if ((!onchainJson && charactersArg) || hasValidRemoteUrls()) {
+    const notOnchainJson = !onchainJson || onchainJson == "null";
+
+  if ((notOnchainJson && charactersArg) || hasValidRemoteUrls()) {
         characters = await loadCharacters(charactersArg);
     }
 
