feca3ffe148228bb7397c0fb94adc729594f7083
AIFlow_ML <mlstudio@aiflow.ml>
2025-01-29 07:46:05 +0700

    Fixed 50% of the typing, To fix all we need to a rewrite the code logic too

--- packages/plugin-nft-generation/src/utils/generateERC721ContractCode.ts
+++ packages/plugin-nft-generation/src/utils/generateERC721ContractCode.ts
@@ -1,7 +1,7 @@
 import solc from "solc";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 // Load OpenZeppelin contract source code
 export function loadOpenZeppelinFile(contractPath) {
@@ -45,7 +45,9 @@ export function compileWithImports(contractName, sourceCode) {
     );
 
     if (output.errors) {
-        output.errors.forEach((err) => console.error(err));
+        for (const err of output.errors) {
+            console.error(err);
+        }
     }
     const contractFile = output.contracts[`${contractName}.sol`][`${contractName}`];
 
