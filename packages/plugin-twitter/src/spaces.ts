1176325964bbd17e0bceade55e6ed0b77b3a24cc
odilitime <janesmith@airmail.cc>
2025-02-18 04:38:03 +0000

    fix case insensitive filename mismatches

--- packages/plugin-twitter/src/spaces.ts
+++ packages/plugin-twitter/src/spaces.ts
@@ -16,7 +16,7 @@ import {
     IdleMonitorPlugin,
     type SpeakerRequest,
 } from "./client/index.ts";
-import { SttTtsPlugin } from "./sttTtsSpacesPlugin.ts";
+import { SttTtsPlugin } from "./stt_tts_spaces_plugin.ts";
 
 interface CurrentSpeakerState {
     userId: string;
