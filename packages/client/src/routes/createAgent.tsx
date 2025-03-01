6e1d53810f079b93d28af3a778bc17e4111a1887
Ting Chien Meng <tcm390@nyu.edu>
2025-03-01 12:32:46 +0800

    clean code

--- packages/client/src/routes/createAgent.tsx
+++ packages/client/src/routes/createAgent.tsx
@@ -1,22 +1,5 @@
 import AgentCreator from "@/components/agent-creator";
 
 export default function AgentCreatorRoute() {
-    // const { agentId } = useParams<{ agentId: UUID }>();
-
-    // const query = useQuery({
-    //     queryKey: ["agent", agentId],
-    //     queryFn: () => apiClient.getAgent(agentId ?? ""),
-    //     // Use polling for real-time updates
-    //     staleTime: STALE_TIMES.FREQUENT,
-    //     refetchInterval: 5000, // Poll every 5 seconds
-    //     enabled: Boolean(agentId),
-    // });
-
-    // if (!agentId) return <div>No data.</div>;
-
-    // const character = query?.data?.character;
-
-    // if (!character) return null;
-
     return <AgentCreator/>;
 }
