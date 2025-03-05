532318efc70cdf1c8d0984ab678437ec5377239f
Sayo <hi@sayo.wtf>
2025-03-05 11:02:33 +0530

    v2 specific init changes

--- packages/cli/src/database.ts
+++
@@ -1,6 +0,0 @@
-import { createDatabaseAdapter } from "@elizaos/plugin-sql";
-
-export const adapter = createDatabaseAdapter({
-  dataDir: process.env.PGLITE_DATA_DIR,
-  postgresUrl: process.env.POSTGRES_URL,
-});
