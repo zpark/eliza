dee3b4e1202cbc070cfc0d21ec3f9ee73d6dc0d5
jin <32600939+madjin@users.noreply.github.com>
2025-03-07 21:19:09 -0500

    docs: Add JSDoc documentation to logger.ts

--- packages/cli/src/utils/logger.ts
+++ packages/cli/src/utils/logger.ts
@@ -1,5 +1,14 @@
 import chalk from "chalk";
 
+/**
+ * Logger object with various logging methods.
+ * @type {Object}
+ * @property {Function} error - Logs an error message in red color.
+ * @property {Function} warn - Logs a warning message in yellow color.
+ * @property {Function} info - Logs an info message in cyan color.
+ * @property {Function} success - Logs a success message in green color.
+ * @property {Function} break - Logs an empty line.
+ */
 export const logger = {
 	error(...args: unknown[]) {
 		console.log(chalk.red(...args));
