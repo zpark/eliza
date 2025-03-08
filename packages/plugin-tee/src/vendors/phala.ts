c2bb7df56fcd777304047a5541f2387a0e12494f
jin <32600939+madjin@users.noreply.github.com>
2025-03-07 21:21:13 -0500

    docs: Add JSDoc documentation to phala.ts

--- packages/plugin-tee/src/vendors/phala.ts
+++ packages/plugin-tee/src/vendors/phala.ts
@@ -4,20 +4,55 @@ import { phalaDeriveKeyProvider as deriveKeyProvider } from "../providers/derive
 import { phalaRemoteAttestationAction as remoteAttestationAction } from "../actions/remoteAttestationAction";
 import { TeeVendors } from "@elizaos/core";
 
+/**
+ * A class representing a vendor for Phala TEE.
+ * * @implements { TeeVendor }
+ * @type {TeeVendors.PHALA}
+ *//**
+ * Get the actions for the PhalaVendor.
+ * * @returns { Array } An array of actions.
+ *//**
+ * Get the providers for the PhalaVendor.
+ * * @returns { Array } An array of providers.
+ *//**
+ * Get the name of the PhalaVendor.
+ * * @returns { string } The name of the vendor.
+ *//**
+ * Get the description of the PhalaVendor.
+ * * @returns { string } The description of the vendor.
+ */
 export class PhalaVendor implements TeeVendor {
 	type = TeeVendors.PHALA;
 
+/**
+ * Returns an array of actions.
+ *
+ * @returns {Array} An array containing the remote attestation action.
+ */
 	getActions() {
 		return [remoteAttestationAction];
 	}
+/**
+ * Retrieve the list of providers.
+ *
+ * @returns {Array<Function>} An array containing two provider functions: deriveKeyProvider and remoteAttestationProvider.
+ */
 	getProviders() {
 		return [deriveKeyProvider, remoteAttestationProvider];
 	}
 
+/**
+ * Returns the name of the plugin.
+ * @returns {string} The name of the plugin.
+ */
 	getName() {
 		return "phala-tee-plugin";
 	}
 
+/**
+ * Get the description of the function
+ * @returns {string} The description of the function
+ */
 	getDescription() {
 		return "Phala TEE Cloud to Host Eliza Agents";
 	}
