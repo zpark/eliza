f471ad7f7aa47654a39700cd6529c8d34326622c
jin <32600939+madjin@users.noreply.github.com>
2025-03-07 21:22:15 -0500

    docs: Add JSDoc documentation to bignumber.ts

--- packages/the-org/src/investmentManager/plugins/community-investor/bignumber.ts
+++ packages/the-org/src/investmentManager/plugins/community-investor/bignumber.ts
@@ -4,6 +4,11 @@ import BigNumber from "bignumber.js";
 export const BN = BigNumber;
 
 // Helper function to create new BigNumber instances
+/**
+ * Converts the input value to a BigNumber.
+ * @param {string | number | BigNumber} value - The value to convert to a BigNumber.
+ * @returns {BigNumber} The BigNumber representation of the input value.
+ */
 export function toBN(value: string | number | BigNumber): BigNumber {
 	return new BigNumber(value);
 }
