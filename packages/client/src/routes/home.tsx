5e417e35dab03a61722afd21b37e35e4b3a78cd2
Ting Chien Meng <tcm390@nyu.edu>
2025-03-13 15:09:58 +0800

    tweak grid size

--- packages/client/src/routes/home.tsx
+++ packages/client/src/routes/home.tsx
@@ -59,7 +59,7 @@ export default function Home() {
 				)}
 
 				{!isLoading && !isError &&(
-					<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+					<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
 						{agents?.sort((a: Agent, b: Agent) => Number(b?.enabled) - Number(a?.enabled)).map((agent: Agent) => {
 							return (
 								<ProfileCard
