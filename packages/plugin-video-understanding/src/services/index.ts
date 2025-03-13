79854c5485b728fdfbbb6d60c42c0b666fe9cd2c
Shaw <shawmakesmagic@gmail.com>
2025-03-12 23:33:22 -0700

    update services etc

--- packages/plugin-video-understanding/src/services/index.ts
+++ packages/plugin-video-understanding/src/services/index.ts
@@ -1,6 +1,3 @@
-import { AwsS3Service } from "./awsS3";
-import { BrowserService } from "./browser";
-import { PdfService } from "./pdf";
 import { VideoService } from "./video";
 
-export { AwsS3Service, BrowserService, PdfService, VideoService };
+export { VideoService };
