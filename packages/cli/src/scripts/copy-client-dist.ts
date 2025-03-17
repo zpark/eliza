7589c715acc182f083fa8732b4427707da080448
Shaw <shawmakesmagic@gmail.com>
2025-03-16 19:29:18 -0700

    remove logs from templates

--- packages/cli/src/scripts/copy-client-dist.ts
+++ packages/cli/src/scripts/copy-client-dist.ts
@@ -7,9 +7,7 @@ import { copyClientDist } from '../utils/copy-template.js';
  * and finally logs a message indicating that the script has been completed.
  */
 async function main() {
-  console.log('Running copy-client-dist script...');
   await copyClientDist();
-  console.log('Script completed');
 }
 
 main().catch((error) => {
