c94a064438b51478dcc105ad462436b6d596c16f
Ting Chien Meng <tcm390@nyu.edu>
2025-03-27 17:00:00 +0800

    discord timeout action and provider

--- packages/the-org/src/communityManager/plugins/communityManager/index.ts
+++ packages/the-org/src/communityManager/plugins/communityManager/index.ts
@@ -1,12 +1,14 @@
 import type { Plugin } from '@elizaos/core';
 import { CommunityManagerService } from './communityService';
+import { timeoutUserProvider } from './providers/timeout';
+import discordTimeoutUser from './actions/discord-timeout';
 
 export const communityManagerPlugin: Plugin = {
   name: 'community-manager',
   description: 'Community Manager Plugin for Eliza',
   evaluators: [],
-  providers: [],
-  actions: [],
+  providers: [timeoutUserProvider],
+  actions: [discordTimeoutUser],
   services: [CommunityManagerService],
 };
 
