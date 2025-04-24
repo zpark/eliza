20eb696c8523a6900c21bc1a3af33dd48a47202f
Odilitime <janesmith@airmail.cc>
2025-04-24 03:06:58 +0000

    load providers as needed, add more providers, warn if not useful

--- packages/the-org/src/investmentManager/plugins/degen-intel/index.ts
+++ packages/the-org/src/investmentManager/plugins/degen-intel/index.ts
@@ -4,13 +4,17 @@ import { registerTasks } from './tasks';
 import { logger } from '@elizaos/core';
 
 import { sentimentProvider } from './providers/sentiment';
+import { cmcMarketProvider } from './providers/cmc_market';
+import { birdeyeTrendingProvider } from './providers/birdeye_trending';
+import { birdeyeTradePortfolioProvider } from './providers/birdeye_wallet';
+// INTEL_SYNC_WALLET provider? or solana handles this?
 
 // create a new plugin
 export const degenIntelPlugin: Plugin = {
   name: 'degen-intel',
   description: 'Degen Intel plugin',
   routes,
-  providers: [sentimentProvider],
+  providers: [],
   tests: [
     {
       name: 'test suite for degen-intel',
@@ -26,5 +30,33 @@ export const degenIntelPlugin: Plugin = {
   ],
   init: async (_, runtime: IAgentRuntime) => {
     await registerTasks(runtime);
+
+    const plugins = runtime.plugins.map((p) => p.name);
+    let notUsed = true;
+
+    // check for cmc key, if have then register provider
+    if (runtime.getSetting('COINMARKETCAP_API_KEY')) {
+      runtime.registerContextProvider(cmcMarketProvider);
+      notUsed = false;
+    }
+
+    // check for birdeeye key, if have then register provider
+    if (runtime.getSetting('BIRDEYE_API_KEY')) {
+      runtime.registerContextProvider(birdeyeTrendingProvider);
+      runtime.registerContextProvider(birdeyeTradePortfolioProvider);
+      notUsed = false;
+    }
+
+    // twitter for sentiment
+    if (plugins.indexOf('twitter') !== -1) {
+      runtime.registerContextProvider(sentimentProvider);
+      notUsed = false;
+    }
+
+    if (notUsed) {
+      logger.warn(
+        'degen-intel plugin is included but not providing any value (COINMARKETCAP_API_KEY/BIRDEYE_API_KEY or twitter are suggested)'
+      );
+    }
   },
 };
