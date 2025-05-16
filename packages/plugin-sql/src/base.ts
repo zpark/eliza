72b240bc663d2486577f02978b9ebf57388f6d51
0xbbjoker <0xbbjoker@proton.me>
2025-05-16 19:14:43 +0200

    fix: Ensure db is initialized in ensureEmbeddingDimension

--- packages/plugin-sql/src/base.ts
+++ packages/plugin-sql/src/base.ts
@@ -182,6 +182,7 @@ export abstract class BaseDrizzleAdapter<
         const usedDimension = Object.entries(DIMENSION_MAP).find(
           ([_, colName]) => existingMemory[0].embedding[colName] !== null
         );
+        // We don't actually need to use usedDimension for now, but it's good to know it's there.
       }
 
       this.embeddingDimension = DIMENSION_MAP[dimension];
