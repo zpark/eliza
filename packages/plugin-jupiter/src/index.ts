b67b8468a6b7b31153e7f1010f52747f28844679
Odilitime <janesmith@airmail.cc>
2025-05-21 00:07:01 +0000

    update name, background wait for chain_solana (also change from solana to chain_solana)

--- packages/plugin-jupiter/src/index.ts
+++ packages/plugin-jupiter/src/index.ts
@@ -3,7 +3,7 @@ import type { Plugin, IAgentRuntime } from '@elizaos/core';
 import { JupiterService } from './services/srv_jupiter';
 
 export const jupiterPlugin: Plugin = {
-  name: 'jupiterOS',
+  name: 'jupiter dex plugin',
   description: 'jupiter plugin',
   actions: [],
   evaluators: [],
@@ -12,25 +12,28 @@ export const jupiterPlugin: Plugin = {
   init: async (_, runtime: IAgentRuntime) => {
     console.log('jupiter init');
 
-    const asking = 'jupiter';
-    const serviceType = 'solana';
-    let solanaService = runtime.getService(serviceType) as any;
-    while (!solanaService) {
-      console.log(asking, 'waiting for', serviceType, 'service...');
-      solanaService = runtime.getService(serviceType) as any;
-      if (!solanaService) {
-        await new Promise((waitResolve) => setTimeout(waitResolve, 1000));
-      } else {
-        console.log(asking, 'Acquired', serviceType, 'service...');
+    new Promise<void>(async (resolve) => {
+      resolve();
+      const asking = 'jupiter';
+      const serviceType = 'chain_solana';
+      let solanaService = runtime.getService(serviceType) as any;
+      while (!solanaService) {
+        console.log(asking, 'waiting for', serviceType, 'service...');
+        solanaService = runtime.getService(serviceType) as any;
+        if (!solanaService) {
+          await new Promise((waitResolve) => setTimeout(waitResolve, 1000));
+        } else {
+          console.log(asking, 'Acquired', serviceType, 'service...');
+        }
       }
-    }
 
-    const me = {
-      name: 'Jupiter DEX services',
-    };
-    solanaService.registerExchange(me);
+      const me = {
+        name: 'Jupiter DEX services',
+      };
+      solanaService.registerExchange(me);
 
-    console.log('jupiter init done');
+      console.log('jupiter init done');
+    });
   },
 };
 
