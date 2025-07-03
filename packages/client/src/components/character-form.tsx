ce688ef0d6726d98a7861e767afbe8c111d5b273
Ting Chien Meng <tcm390@nyu.edu>
2025-07-03 10:58:44 +0800

    fix: reverse export/import icon

--- packages/client/src/components/character-form.tsx
+++ packages/client/src/components/character-form.tsx
@@ -958,11 +958,11 @@ export default function CharacterForm({
             </DropdownMenuTrigger>
             <DropdownMenuContent align="start" side="top">
               <DropdownMenuItem onClick={handleExportJSON}>
-                <ArrowDownToLine className="h-4 w-4 mr-2" />
+                <ArrowUpFromLine className="h-4 w-4 mr-2" />
                 Export
               </DropdownMenuItem>
               <DropdownMenuItem onClick={handleImportClick}>
-                <ArrowUpFromLine className="h-4 w-4 mr-2" />
+                <ArrowDownToLine className="h-4 w-4 mr-2" />
                 Import
               </DropdownMenuItem>
               {stopDeleteOptions.length > 0 && <DropdownMenuSeparator />}
